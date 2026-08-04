fef6eef55c31684b9aeed10be3cee1d6401fd0d6
williamwinter2008-prog <williamwinter2008@gmail.com>
2026-08-04 18:03:52 +0100

    Update me.js

--- api/me.js
+++ api/me.js
@@ -1,36 +1,57 @@
 module.exports = async function handler(req, res) {
 
-    const cookies = req.headers.cookie || "";
+    const cookies =
+        req.headers.cookie || "";
 
-    const match = cookies.match(
-        /pixelbot_user=([^;]+)/
-    );
+    const match =
+        cookies.match(
+            /pixelbot_user=([^;]+)/
+        );
+
 
     if (!match) {
-        return res.status(401).json({
-            error: "Not logged in"
-        });
+
+        return res
+            .status(401)
+            .json({
+                error:
+                    "Not logged in"
+            });
     }
+
 
     try {
 
-        const user = JSON.parse(
-            decodeURIComponent(match[1])
-        );
+        const user =
+            JSON.parse(
+                Buffer
+                    .from(
+                        match[1],
+                        "base64"
+                    )
+                    .toString("utf8")
+            );
 
-        return res.status(200).json(user);
 
-    } catch (error) {
+        return res
+            .status(200)
+            .json(user);
+
+    }
+
+    catch (error) {
 
         console.error(
-            "User data error:",
+            "Cookie error:",
             error
         );
 
-        return res.status(500).json({
-            error: "Could not load user"
-        });
-
+        return res
+            .status(500)
+            .json({
+                error:
+                    "Invalid login"
+            });
     }
 
 };
